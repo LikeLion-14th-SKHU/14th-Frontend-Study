fd6a57505cfbf6cf2f2a5d6490d02217f84ae6aa
rimedang <arim0930@gmail.com>
2026-02-01 23:30:17 +0900

    ar section1 ~ 2.7 (여섯번째 주 학습)

---
+++ Tailwind/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+/**이거 안하면 단축키? 안보이므로 이걸 꼭 입력해야함. */
