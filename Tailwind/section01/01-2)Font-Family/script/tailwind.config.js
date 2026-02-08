d6754cfc065eeb347b2e5b2099fd779b523cd904
dz-1010 <qwop_01@naver.com>
2026-02-08 21:42:42 +0900

    [8주차] Dz Tailwind ~2.12

---
+++ Tailwind/section01/01-2)Font-Family/script/tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ['Noto Sans KR', 'Arial', 'sans-serif'],
+                'single': ["Single Day", 'cursive'],
+                'blackhan': ["Black Han Sans", 'sans-serif'],
+                'montserrat': ["Montserrat", 'sans-serif'],
+                'yclover': ["YClover"]
+            }
+        }
+    }
+}
